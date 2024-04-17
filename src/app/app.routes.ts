a5bf25c855a1713c7f9ec23f99feb2666a9f6c57
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-16 22:41:42 -0500

    Refactor component name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'field',
-    loadComponent: () => import('./field/field.component'),
+    loadComponent: () => import('./business/business.component'),
   },
   {
     path: '**',
--- src/app/business/business.component.ts
+++ src/app/business/business.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   imports: [],
-  templateUrl: './field.component.html',
+  templateUrl: './business.component.html',
   styles: ``
 })
-export default class FieldComponent {
+export default class BusinessComponent {
 
 }
