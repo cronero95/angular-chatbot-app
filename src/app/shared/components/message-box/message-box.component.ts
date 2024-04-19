1ccb245d4221f0314ee42e1625c00fecb4fd8b01
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-19 17:23:56 -0500

    Create message component to contain the messages

--- src/app/shared/components/message-box/message-box.component.ts
+++ src/app/shared/components/message-box/message-box.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { PanelModule } from 'primeng/panel';
+
+import { UserResponse } from '../../../interfaces/request-response.interface';
 
 @Component({
   selector: 'app-message-box',
   standalone: true,
-  imports: [],
+  imports: [
+    PanelModule,
+  ],
   templateUrl: './message-box.component.html',
   styles: ``
 })
 export class MessageBoxComponent {
+
+  @Input() message?: UserResponse;
 
 }
