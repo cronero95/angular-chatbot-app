3032464b89d676ada89ab4e13798083afed248e6
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-19 17:05:23 -0500

    Clean chatbot component

--- src/app/shared/chatbot/chatbot.component.ts
+++ src/app/shared/chatbot/chatbot.component.ts
@@ -1,10 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { MessageService } from '../../services/message.service';
-import { MessageSent } from '../../interfaces/request.interface';
-import { UserResponse } from '../../interfaces/request-response.interface';
+
 import { MessageFormComponent } from '../components/message-form/message-form.component';
+
+import { UserResponse } from '../../interfaces/request-response.interface';
 
 @Component({
   selector: 'shared-chatbot',
@@ -22,18 +24,5 @@ export class ChatbotComponent {
   private readonly messageService = inject(MessageService);
 
   public messageHistory = computed<UserResponse[]>(() => this.messageService.messageHistorySignal());
-
-  sendMessage(message: string): void {
-
-    const messageSent: MessageSent = {
-      content: message,
-    };
-
-    this.messageService.onMessageSent(messageSent);
-  }
-
-  deleteChat(): void {
-    this.messageService.onDeleteChat();
-  }
 
 }
